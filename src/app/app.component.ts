dbacbcdb6d80aa84dccbf4dc9cd220cdb2397f5d
jan.blazek <JBlazekk@seznam.cz>
2019-03-14 21:57:54 +0100

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+
+class Info {
+  public header: string;
+  public description: string;
+  public isOK: boolean;
+
+
+  constructor(header: string, description: string, isOK: boolean) {
+    this.header = header;
+    this.description = description;
+    this.isOK = isOK;
+  }
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +20,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'bmi';
+
+  public form: FormGroup;
+  public info: Info;
+
+  constructor() {
+    this.form = this.buildForm();
+  }
+
+  public onSubmit(): void {
+    const values = this.form.value;
+    const bmi = this.calculateBMI(values.weight, values.height);
+    this.info = this.buildInfo(bmi);
+  }
+
+  private buildInfo(mbi: number): Info {
+    if (mbi > 40) {
+      return new Info('Nadháva III. stupně!', 'Měl bys zhubnout.', false);
+    } else if (mbi > 35) {
+      return new Info('Nadváha II. stupně!', 'Měl bys zhubnout.', false);
+    } else if (mbi > 30) {
+      return new Info('Nadváha I. stupně!', 'Měl bys zhubnout.', false);
+    } else if (mbi > 25) {
+      return new Info('Nadváha', 'Měl bys zhubnout.', false);
+    } else if (mbi > 18) {
+      return new Info('Normální váha', 'Jsi v pohodě.', true);
+    }
+
+    return new Info('Podváha', 'Měl bys začít pořádně jíst.', false);
+  }
+
+  private calculateBMI(weight: number, height: number): number {
+    const meters = height / 100;
+    return weight / (meters * meters);
+  }
+
+  private buildForm(): FormGroup {
+    return new FormGroup({
+      weight: new FormControl(null, [Validators.required, Validators.min(1)]),
+      height: new FormControl(null, [Validators.required, Validators.min(1)])
+    });
+  }
 }
